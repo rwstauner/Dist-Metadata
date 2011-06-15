2ae20e27fd73f56ff53de656f96ee34e8faa1511
Randy Stauner <randy@magnificent-tears.com>
2011-06-15 13:39:28 -0700

    Add Abstract, Synopsis, and Description pod

--- lib/Dist/Metadata.pm
+++ lib/Dist/Metadata.pm
@@ -3,12 +3,35 @@ use strict;
 use warnings;
 
 package Dist::Metadata;
-# ABSTRACT: undef
+# ABSTRACT: Information about a perl module distribution
 
 1;
 
+=for :stopwords dist
+
+=for test_synopsis
+my $path_to_archive;
+
 =head1 SYNOPSIS
 
+  my $dist = Dist::Metadata->new(file => $path_to_archive);
+  my $name = $dist->name;
+  my $version = $dist->version;
+
+  my $provides = $dist->module_versions;
+  while( my ($module, $version) = each %$provides ){
+    print "Dist $name ($version) includes $module $version\n";
+  }
+
 =head1 DESCRIPTION
+
+This is sort of a companion to L<Module::Metadata>.
+It provides an interface for getting information about a distribution.
+
+This is mostly a wrapper around L<CPAN::Meta>
+providing an easy interface to find and load the meta file from a F<tar.gz> file.
+
+If the dist does not contain a meta file
+the module will attempt to determine some of that data from the dist.
 
 =cut
