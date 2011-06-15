ae4d0976eb0a4d0492184e6f241a8a9d4807e7fe
Randy Stauner <randy@magnificent-tears.com>
2011-06-15 13:43:45 -0700

    Define basic functionality: load CPAN Meta file

--- lib/Dist/Metadata.pm
+++ lib/Dist/Metadata.pm
@@ -5,6 +5,108 @@ use warnings;
 package Dist::Metadata;
 # ABSTRACT: Information about a perl module distribution
 
+use Carp qw(croak);
+use CPAN::Meta;
+use Archive::Tar;
+use List::Util qw(first);    # core in perl v5.7.3
+
+#use Module::Metadata;
+
+=method new
+
+  Dist::Metadata->new(file => $path);
+
+Constructor.
+
+Accepts a single 'file' argument that should be a path to a F<tar.gz> file.
+
+=cut
+
+sub new {
+  my $class = shift;
+  my $self  = {
+    @_ == 1 ? %{ $_[0] } : @_
+  };
+
+  bless $self, $class;
+
+  croak q['file' parameter not supplied]
+    unless $self->{file} || $self->{archive};
+
+  return $self;
+}
+
+=method archive
+
+Returns the archive object (loaded from L</file>).
+
+=cut
+
+sub archive {
+  my ($self) = @_;
+  return $self->{archive} ||= do {
+    my $file = $self->file;
+
+    croak "File '$file' does not exist"
+      unless -e $file;
+
+    my $tar = Archive::Tar->new();
+    $tar->read($file);
+
+    $tar; # return
+  };
+}
+
+=method file
+
+Returns the 'file' parameter passed to the constructor.
+Should be the path to an archive file.
+
+=cut
+
+sub file {
+  return $_[0]->{file};
+}
+
+=method load_meta
+
+Loads the metadata from the L</file>.
+
+=cut
+
+sub load_meta {
+  my ($self) = @_;
+
+  my $archive = $self->archive;
+  my @files   = $archive->list_files;
+  my ($meta, $metafile);
+
+  # prefer json file
+  if ( $metafile = first { m#^([^/]+/)?META\.json$# } @files ) {
+    $meta = CPAN::Meta->load_json_string( $archive->get_content($metafile) );
+  }
+  elsif ( $metafile = first { m#^([^/]+/)?META\.ya?ml$# } @files ) {
+    $meta = CPAN::Meta->load_yaml_string( $archive->get_content($metafile) );
+  }
+  # no META file found in archive
+  else {
+    croak('TODO: determine basic metadata when META file not found');
+  }
+
+  return $meta;
+}
+
+=method meta
+
+Returns the L<CPAN::Meta> instance in use.
+
+=cut
+
+sub meta {
+  my ($self) = @_;
+  return $self->{meta} ||= $self->load_meta;
+}
+
 1;
 
 =for :stopwords dist
